a6787188f2538435b753b5bc2419c09b1cc63b10
manjujidagi <manjujidagi@protonmail.com>
2026-05-20 22:52:39 +0530

    Updated error messages

--- src/middlewares/authenticate.middleware.js
+++ src/middlewares/authenticate.middleware.js
@@ -2,7 +2,7 @@
 const authenticate = (req, res, next) => {
   const apiKey = req.header('X-API-Key');
   if (!apiKey || apiKey !== process.env.API_KEY) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ error: 'Unauthorized', error_code: 'UNAUTHORIZED' });
   }
   next();
 };
