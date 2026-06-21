878c952be3a9032e2f0b91b47847bb1cc9dae0da
naveengouda091 <bevinamaradnandishgouda@gmail.com>
2026-06-21 16:40:48 +0530

    fetching individual email details

--- src/routes/imap.routes.js
+++ src/routes/imap.routes.js
@@ -1,11 +1,27 @@
 import { Router } from "express";
-import { getEmails } from "../methods/imap.methods.js";
+import { getEmails,getEmail } from "../methods/imap.methods.js";
 
 const router = Router();
 
 router.get("/emails", async (req, res) => {
   try {
     const result = await getEmails(req.decryptedData, req.query);
+
+    return res.status(200).json(result);
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+});
+
+router.get("/emails/:email_id", async (req, res) => {
+  try {
+    const result = await getEmail(
+      req.decryptedData,
+      req.params.email_id
+    );
 
     return res.status(200).json(result);
   } catch (error) {
